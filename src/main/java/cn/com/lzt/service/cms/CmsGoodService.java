23a0e49b22989fd9cad538a03d290de4bc222ec3
wanghao2 <wanghao2@gomeholdings.com>
2017-07-20 17:59:31 +0800

    增加添加商品接口

---
+++ src/main/java/cn/com/lzt/service/cms/CmsGoodService.java
@@ -0,0 +1,11 @@
+package cn.com.lzt.service.cms;
+
+import cn.com.lzt.common.ResponseMessage;
+import cn.com.lzt.model.TCmsGood;
+
+/**
+ * Created by Administrator on 2017/7/20.
+ */
+public interface CmsGoodService {
+    ResponseMessage save(TCmsGood tCmsGood);
+}
---
+++ src/main/java/cn/com/lzt/service/cms/CmsGoodServiceImpl.java
@@ -0,0 +1,30 @@
+package cn.com.lzt.service.cms;/**
+ * Created by Administrator on 2017/7/20.
+ */
+
+import cn.com.lzt.common.ResponseMessage;
+import cn.com.lzt.mapper.TCmsGoodMapper;
+import cn.com.lzt.model.TCmsGood;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * cms商品service
+ *
+ * @author WangH
+ * @create 2017-07-20 17:55
+ **/
+@Service
+@Transactional
+public class CmsGoodServiceImpl implements CmsGoodService {
+    
+    private TCmsGoodMapper tCmsGoodMapper;
+
+
+    @Override
+    public ResponseMessage save(TCmsGood tCmsGood) {
+        ResponseMessage rm=null;
+         tCmsGoodMapper.insertSelective(tCmsGood);
+        return rm;
+    }
+}
